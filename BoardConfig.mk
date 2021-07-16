18e1dd1ed608b98820d7c65718ea7959e3328fb0
Echo2124 <echo2124@hotmail.com>
2021-07-17 09:14:43 +1000

    Push changes from DarkAngel's fork

---
+++ BoardConfig.mk
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sdm710-common
+-include device/xiaomi/sdm710-common/BoardConfigCommon.mk
+
+BUILD_BROKEN_DUP_RULES := true
+
+DEVICE_PATH := device/xiaomi/pyxis
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := pyxis
+
+# AVB
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 2
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# FM
+BOARD_HAS_QCA_FM_SOC := "cherokee"
+BOARD_HAVE_QCOM_FM := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := pyxis_defconfig
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+# NFC
+TARGET_USES_NQ_NFC := true
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3758096384
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 119334219776
+BOARD_VENDORIMAGE_PARTITION_SIZE := 1610612736
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/pyxis/BoardConfigVendor.mk
