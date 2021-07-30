9b1b509032d7c34dd9fab762074fe1401cb36b36
Echo2124 <echo2124@hotmail.com>
2021-07-30 21:57:42 +1000

    Remove fingerprint and trustzone requirements

--- dot_pyxis.mk
+++ dot_pyxis.mk
@@ -13,7 +13,9 @@ $(call inherit-product, vendor/dot/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 DEVICE_MAINTAINER := Echo2124
-
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
+WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_pyxis
@@ -22,10 +24,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V12.0.6.0.QFCMIXM:user/release-keys"
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pyxis-user 11 RKQ1.200826.002 V12.5.1.0.RFCCNXM release-keys" \
-    PRODUCT_NAME="pyxis"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
